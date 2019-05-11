0920ed6096fba11b46b03ef5fc29ae6ee19af498
RumblingBee <m.dezette@gmail.com>
2019-05-11 18:41:13 +0200

    Catalogue et liste Albums fonctionnel

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
 /**
  * URL of the called webservice
  */
-$webserviceURL="http://127.0.0.1:3000/";
+webserviceURL = "http://127.0.0.1:3000/";
 
 
 /**
  * methods called
  */
-$urlFindAll = $webserviceURL + "albums";
-$urlCatalog = $webserviceURL + "catalog";
+urlAlbums = webserviceURL + "albums";
+urlCatalogs = webserviceURL + "catalog";
 
-$url = "";
+urlInit = "";
+urlCalled = "";
 
 
 // we initialize the value of the offset to 0
-$offset = 0;
+offset = 0;
 
 // the number of results shown
-$limit = 24;
+limit = 24;
+
+
+function initAlbums() {
+
+    urlInit = urlAlbums;
+
+    limit = 24;
+    offset = 0;
+
+    showResults();
+
+    $('.hidden').removeClass('hidden');
+}
+
+
+function initCatalogs() {
+
+    urlInit = urlCatalogs;
+
+    limit = 4;
+    offset = 0;
+
+
+    removeTableResults();
+
+
+    showResults();
+
+    $('.hidden').removeClass('hidden');
+};
+
+
+
 
 
 /**
@@ -29,96 +63,49 @@ $limit = 24;
 function showResults() {
 
     removeTableResults();
-	
-	$url = $urlFindAll + '/' + $offset + '/' + $limit;
-
-    renderTableResults()
+    buildUrl();
+    processQuery()
 };
 
-
 /**
- * Return the data of the people's searshed
- * this function calls different's webservice's methods
- * depending on the informations provided by the user?
- * 
+ * Renders the results in  HTML's page
  */
-function showCatalog() {
-    $offset = 0;
-    $limit = 4; 
+function processQuery() {
 
-    removeTableResults();
-	
-	$url = $urlCatalog + '/' + $offset + '/' + $limit;;
+    $.ajax({
+        url: urlCalled
+    }).then(function (data) {
+        renderResult(data);
 
-    renderTableResults()
+    });
 
-    $('.hidden').removeClass('hidden');
-};
+}
+
+
 
 
 /**
- * Returns the next $limit peoples
+ * Returns the next limit peoples
  */
 function showNextResults() {
 
-    $offset = $offset + $limit;
+    offset = offset + limit;
     showResults();
     $('#previous-Btn').show();
 }
 
 /**
- * Returns the previous $limit peoples
+ * Returns the previous limit peoples
  */
-function showPreviousResults(){
-if($offset != 0){
-    $offset = $offset - $limit;
-    showResults();
-    $('#next-Btn').show();
-    
-}
- 
-/**
- * Returns the first results when the user
- * presses "Searsh" for the first time
- * 
- */
-}
-function showFirstResults(){
-    $limit = 24;
-    $offset = 0;
-    showResults();
-    $('.hidden').removeClass('hidden');
+function showPreviousResults() {
+    if (offset != 0) {
+        offset = offset - limit;
+        showResults();
+        $('#next-Btn').show();
+
+    }
 }
 
-
-/**
- * Renders the results in  HTML's page
- */
-function renderTableResults() {
-
-    $.ajax({
-        url: $url
-    }).then(function (data) {
-        $.each(data, function (index, value) {
-			console.log(data[index]);
-            $('#tblData').append(
-                '<div class="col-lg-1" style="height:250px">' 
-                + '<div style="background-image: url(\'https://spark.adobe.com/images/landing/examples/design-music-album-cover.jpg\');height:50%;background-size: cover"></style></div>'
-                +'<p>'+ data[index].artiste +'</br>'
-                + data[index].nom +'</br>'
-                + data[index].annee +'</p>'
-            + '</div>'
-        );})
-     if(data.length < $limit){
-        $('#next-Btn').hide();
-     }
-     if($offset === 0){
-        $('#previous-Btn').hide();
-     }
-
-    });
-
-}
 
 /**
  * Clear the table rendered
@@ -130,5 +117,48 @@ function removeTableResults() {
     ).appendTo('#results');
 }
 
+function renderResult(data) {
 
 
+    if (urlInit === urlAlbums) {
+
+        $.each(data, function (index, value) {
+            console.log(data[index]);
+            $('#tblData').append(
+                '<div class="col-lg-3" style="height:250px">' +
+                '<div style="background-image: url(\'https://spark.adobe.com/images/landing/examples/design-music-album-cover.jpg\');height:50%;background-size: cover"></style></div>' +
+                '<p>' + data[index].artiste + '</br>' +
+                data[index].nom + '</br>' +
+                data[index].annee + '</p>' +
+                '</div>'
+            );
+        })
+    } else if (urlInit === urlCatalogs) {
+        $.each(data, function (index, value) {
+            console.log(data[index]);
+            $('#tblData').append(
+                '<div class="col-lg-3" style="height:250px">' +
+                '<div style="background-image: url(\'https://spark.adobe.com/images/landing/examples/design-music-album-cover.jpg\');height:50%;background-size: cover"></style></div>' +
+                '<p>' + data[index].nom + '</br>' +
+                data[index].artiste + '</br>' +
+                data[index].prix + '€ </br>' +
+                '</div>'
+            );
+        })
+
+    } else if (urlInit.includes("artist")) {
+
+    }
+
+    if (data.length < limit) {
+        $('#next-Btn').hide();
+    }
+    if (offset === 0) {
+        $('#previous-Btn').hide();
+    }
+
+}
+
+function buildUrl() {
+    urlCalled = urlInit + '/' + offset + '/' + limit;
+}
